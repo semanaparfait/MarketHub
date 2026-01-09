21ef35411a283745ec10fc7b4ac6747283d56303
semanaparfait <p.semana@alustudent.com>
2026-01-09 09:23:40 +0200

    structing folder structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+function Home() {
+  return (
+    <section>
+        <div>
+            <Navbar/>
+        </div>
+        <div>
+            <Footer/>
+        </div>
+    </section>
+  )
+}
+
+export default Home
